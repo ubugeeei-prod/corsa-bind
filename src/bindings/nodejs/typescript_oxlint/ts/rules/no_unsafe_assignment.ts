d382ce1d8d8b614221ddd5b0f2257cee1c78d505
ubugeeei <ubuge1122@gmail.com>
2026-05-23 02:03:11 +0900

    perf(corsa-oxlint): filter native unsafe bridge inputs

--- src/bindings/nodejs/typescript_oxlint/ts/rules/no_unsafe_assignment.ts
+++ src/bindings/nodejs/typescript_oxlint/ts/rules/no_unsafe_assignment.ts
@@ -1,3 +1,22 @@
 import { createRustNativeRule } from "./native_bridge";
 
-export const noUnsafeAssignmentRule = createRustNativeRule("no-unsafe-assignment");
+export const noUnsafeAssignmentRule = createRustNativeRule(
+  "no-unsafe-assignment",
+  {},
+  {
+    shouldRun: shouldRunNoUnsafeAssignment,
+  },
+);
+
+function shouldRunNoUnsafeAssignment(node: any): boolean {
+  switch (node.type) {
+    case "AssignmentExpression":
+      return node.operator === "=" && Boolean(node.right);
+    case "PropertyDefinition":
+      return Boolean(node.value);
+    case "VariableDeclarator":
+      return Boolean(node.init);
+    default:
+      return true;
+  }
+}
