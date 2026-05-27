a6fceedff03328bcaaf5be392d938d0e1c6e7942
ubugeeei <ubuge1122@gmail.com>
2026-05-27 20:15:55 +0900

    fix: widen binding identifier type annotations

--- src/bindings/nodejs/corsa_oxlint/ts/index.ts
+++ src/bindings/nodejs/corsa_oxlint/ts/index.ts
@@ -1,3 +1,5 @@
+import "./oxlint_plugins";
+
 export { AST_NODE_TYPES, AST_TOKEN_TYPES, TSESTree } from "./compat";
 export * as ASTUtils from "./ast_utils";
 export * as JSONSchema from "./json_schema";
