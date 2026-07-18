d7a9bb59e280b7bf49105f06943f4ca9f0f612ea
ubugeeei <ubuge1122@gmail.com>
2026-07-18 22:20:19 +0900

    release: v1.0.0-beta.5

--- src/bindings/elixir/corsa_utils/mix.exs
+++ src/bindings/elixir/corsa_utils/mix.exs
@@ -1,7 +1,7 @@
 defmodule CorsaUtils.MixProject do
   use Mix.Project
 
-  @version "1.0.0-beta.4"
+  @version "1.0.0-beta.5"
   @source_url "https://github.com/ubugeeei-prod/corsa-bind"
 
   def project do
